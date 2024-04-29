b34bbaaf896bfb40f094f46d0b449b278f524f91
Mahmoud Elbahrawy <58396137+bahrawycodo@users.noreply.github.com>
2024-04-30 00:15:29 +0300

    jj

---
+++ Demo.DAL/Context/AppDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Demo.DAL.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.DAL.Context
+{
+    public class AppDbContext:IdentityDbContext<ApplicationUser,ApplicationRole,string>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseSqlServer("server=.; database=CompanyAppDb; integrated security=true;");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);                        
+           modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+        public DbSet<Department> Departments { get; set; }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
